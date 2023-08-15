184c07b9a2afd2786281b30db19cc865b0d3a48c
Tarek Maza <tarekmaza1.0@gmail.com>
2023-08-15 18:08:40 +0100

    docker-compose file updated

--- src/config/connectToDB.ts
+++ src/config/connectToDB.ts
@@ -5,17 +5,17 @@ async function connectToDatabase() {
     const DB_PASSWORD = process.env.DB_PASSWORD;
     const DB_HOST = process.env.DB_HOST;
     const DB_PORT = process.env.DB_PORT;
-    const DB_NAME = process.env.DB_NAME;
+    const DB_DATABASE = process.env.DB_DATABASE;
 
     if (mongoose.connections[0].readyState) {
         console.log('Already Connected');
     } else {
         console.log(
-            `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+            `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
         );
         try {
             await mongoose.connect(
-                `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+                `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
             );
             console.log('Connected to MongoDB');
         } catch (err) {
@@ -23,5 +23,6 @@ async function connectToDatabase() {
         }
     }
 }
+// ?authSource=admin
 
 export default connectToDatabase;
